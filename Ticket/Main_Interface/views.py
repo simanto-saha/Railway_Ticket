fe7af6d119c86df86692973b30cf7cc884ff05ab
simanto-saha <simanto.saha2167@gmail.com>
2026-09-21 22:26:21 +0600

    home_page added

--- Ticket/Main_Interface/views.py
+++ Ticket/Main_Interface/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+
+
+
+
+
+
+def home_page(request):
+    return render(request, "Main_Interface/home_page.html")
+
