da5659fee4a7a7bc2b815da536b54b265d7f7a78
simanto-saha <simanto.saha2167@gmail.com>
2026-09-24 20:59:11 +0600

    superuser log in added

--- Ticket/Railway_Admin/urls.py
+++ Ticket/Railway_Admin/urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = []
+urlpatterns = [
+    path('superuser-dashboard/IT-Division/', views.superuser_dashboard, name='superuser_dashboard'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+
+    path('superuser-login/', views.superuser_login, name='superuser_login'),
+    path('superuser-logout/', views.superuser_logout, name='superuser_logout'),
+]
--- Ticket/Railway_Admin/views.py
+++ Ticket/Railway_Admin/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def superuser_dashboard(request):
+    return render(request, 'Railway_Admin/superuser_dashboard.html')
+
+
+def superuser_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(username=username).first()
+
+        if user and user.check_password(password) and user.is_superuser:
+            login(request, user)
+            return JsonResponse({
+                'success': True,
+                'redirect_url': reverse('superuser_dashboard')
+            })
+        else:
+            return JsonResponse({
+                'success': False,
+                'error': 'Invalid credentials or not a superuser.'
+            })
+
+    return render(request, 'Railway_Admin/superuser_login.html')
+
+
+def superuser_logout(request):
+    logout(request)
+    return redirect('superuser_dashboard')
+
+
+def admin_dashboard(request):
+    return render(request, 'Railway_Admin/admin_dashboard.html')
